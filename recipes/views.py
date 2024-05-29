d01cb224d0bf397fbbc6950d52431775a78ec3a9
gilbertoloboac <loboacre@gmail.com>
2024-05-28 21:26:18 -0300

    atualization 28-05-2024

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
